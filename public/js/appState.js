71216a2df27ea332fd4fae8bb8dd58215fd986ce
Benjamin Dirgo <bdirgo@gmail.com>
2021-03-31 11:29:45 -0700

    sort browse section

--- public/js/appState.js
+++ public/js/appState.js
@@ -616,6 +616,19 @@ async function bookLibraryReducer(state = [], action) {
                         length: val.entries.length,
                     }
                 })
+                .sort(function(a, b) {
+                    var nameA = a.title.toUpperCase(); // ignore upper and lowercase
+                    var nameB = b.title.toUpperCase(); // ignore upper and lowercase
+                    if (nameA < nameB) {
+                      return -1;
+                    }
+                    if (nameA > nameB) {
+                      return 1;
+                    }
+                  
+                    // names must be equal
+                    return 0;
+                });
             return bookLibrary;
         }
         case('new-tab'): {
