143fe3fba88a0367f83dada077fc74f382eb21fc
Benjamin Dirgo <bdirgo@gmail.com>
2022-04-27 09:03:36 -0700

    add user preference to dark mode

--- public/js/darkMode.js
+++ public/js/darkMode.js
@@ -1,5 +1,5 @@
 import {populateStorage, setStyles, toggleDarkMode} from './utility.js'
-import {Moon, darkModeID, light} from './constats.js'
+import {Moon, darkModeID, light, dark} from './constats.js'
 
 let body = document.getElementsByTagName("body")[0]
 let darkButton = document.getElementById("dark-button")
@@ -8,6 +8,23 @@ if(typeof(darkButton) === 'undefined' || darkButton === null){
 }
 darkButton.addEventListener("click", () => toggleDarkMode(body, darkButton), false);
 darkButton.innerHTML = Moon;
+const handlePreferenceChange = (mql) => {
+    if (mql.matches) {
+        populateStorage(darkModeID, dark);
+        setStyles(body, darkButton);
+    } else {
+        populateStorage(darkModeID, light);
+        setStyles(body, darkButton);
+    }
+}
+const mediaPreference = window.matchMedia('(prefers-color-scheme: dark)');
+if (mediaPreference.media !== 'not all') {
+    console.log("🎉 Dark mode supported!")
+    mediaPreference.addEventListener('change', handlePreferenceChange);
+    handlePreferenceChange(mediaPreference);
+} else {
+    console.log("💩")
+}
 if(!window.localStorage.getItem(darkModeID)) {
     populateStorage(darkModeID, light);
 }
